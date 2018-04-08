3ce81e8d57cac15d35934efadee154b74d35ccd2
Jesse Boyce <jesse@jpboyce.org>
2018-04-08 15:24:23 +1000

    FC008, FC064, FC065, FC067, FC078 fixes

--- metadata.rb
+++ metadata.rb
@@ -1,20 +1,21 @@
 name 'cb-cis-windows-10'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
-license 'All Rights Reserved'
+maintainer 'Jesse Boyce'
+maintainer_email 'jesse@jpboyce.org'
+license 'MIT'
 description 'Installs/Configures cb-cis-windows-10'
 long_description 'Installs/Configures cb-cis-windows-10'
 version '0.1.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
+supports windows
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
-# issues_url 'https://github.com/<insert_org_here>/cb-cis-windows-10/issues'
+issues_url 'https://github.com/jpboyce/cb-cis-windows-10/issues'
 
 # The `source_url` points to the development repository for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
-# source_url 'https://github.com/<insert_org_here>/cb-cis-windows-10'
+source_url 'https://github.com/jpboyce/cb-cis-windows-10'
