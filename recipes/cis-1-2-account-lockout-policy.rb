40971fd8cf4b5d3d458bd88b6bfcaf3ce56a78bb
Jesse Boyce <jesse@jpboyce.org>
2018-04-08 15:28:28 +1000

    cookstyle fix - missing space after #

--- recipes/cis-1-2-account-lockout-policy.rb
+++ recipes/cis-1-2-account-lockout-policy.rb
@@ -9,15 +9,15 @@ security_policy 'Account lockout duration' do
 end
 
 # Ensure 'Account lockout threshold' is set to '10 or fewer invalid logon attempt(s), but not 0'
-#security_policy 'Account lockout threshold' do
+# security_policy 'Account lockout threshold' do
 #  policy_template "#{node['cb_cis_windows_10']['secedit_template']['location']}\\Account lockout threshold.inf}"
 #  database "#{node['cb_cis_windows_10']['secedit_database']['location']}\\#{['cb_cis_windows_10']['secedit_database']['name']}"
 #  action :configure
-#end
+# end
 
 # Ensure 'Reset account lockout counter after' is set to '15 or more minute(s)'
-#security_policy 'Reset account lockout counter after' do
+# security_policy 'Reset account lockout counter after' do
 #  policy_template "#{node['cb_cis_windows_10']['secedit_template']['location']}\\Reset account lockout counter after.inf}"
 #  database "#{node['cb_cis_windows_10']['secedit_database']['location']}\\#{['cb_cis_windows_10']['secedit_database']['name']}"
 #  action :configure
-#end
+# end
